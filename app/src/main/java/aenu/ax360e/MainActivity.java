6bcd2081b0e9a1466485f67b5477c474575f914b
zay448345045 <raknet@163.com>
2026-04-08 20:35:04 +0800

    更新 MainActivity.java

--- app/src/main/java/aenu/ax360e/MainActivity.java
+++ app/src/main/java/aenu/ax360e/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
         String config_str=Emulator.get.generate_config_xml(new File(Application.get_app_data_dir(),"xenia.config.toml").getAbsolutePath());
         File config_file=new File(Application.get_app_data_dir(),"xenia.config.xml");
         Utils.save_string(config_file,config_str);*/
-    }
+//    }
 /*
     void on_create(){com.google.android.gms.ads.MobileAds.initialize(this, initializationStatus -> {
 final AppOpenAdManager app_open_ad_manager=AppOpenAdManager.getInstance(this);
